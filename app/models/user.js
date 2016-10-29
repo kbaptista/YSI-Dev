09a0675e1a705a663803c836542456fa0cc2ddf4
timotheepicard <ptim64@gmail.com>
2016-10-29 14:55:20 +0200

    kanban sprint1 tasks

--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,23 @@
-/**
- * Created by Timothée on 20/10/2016.
- */
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = new mongoose.Schema();
+userSchema.add({
+    name: String,
+    email: String,
+    password: String,
+    idProjects: []
+});
+
+userSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password,bcrypt.genSaltSync(8),null);
+};
+
+userSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password,this.password);
+};
+
+var User = mongoose.model('User',userSchema);
+
+exports.schema = userSchema;
+exports.model = User;
