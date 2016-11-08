ff460ef490ba9072384f13beed735fb0cff7e558
timotheepicard <ptim64@gmail.com>
2016-11-08 16:30:20 +0100

    connexion (localstorage) + inscription Ok

--- public/js/controllers/register.controller.js
+++ public/js/controllers/register.controller.js
@@ -1,29 +1,14 @@
-angular.module('RegisterCtrl',[]).controller('RegisterController', function($http,$scope){
-    $scope.postForm = function(){
-        var encodedString = '';
-        if($scope.input.email){
-            encodedString += 'email=' + encodeURIComponent($scope.input.email);
-        }
-        if($scope.input.name){
-            encodedString += '&name=' + encodeURIComponent($scope.input.name);
-        }
-        if($scope.input.password){
-            encodedString += '&password=' + encodeURIComponent($scope.input.password);
-        }
-        /*encodedString = 'email=' + encodeURIComponent($scope.input.email) + '&name=' + encodeURIComponent($scope.input.name) +
-                '&password=' + encodeURIComponent($scope.input.password);*/
-
-        $http({
-            method: 'POST',
-            url: 'http://localhost:3000/signup',
-            data: encodedString,
-            headers : {'Content-Type' : 'application/x-www-form-urlencoded'}
-        })
-            .success(function(data,status,headers,config){
-                
-            })
-            .error(function(data,status,headers,config){
-                console.log('unable to submit the form');
-            });
+angular.module('RegisterCtrl',[]).controller('RegisterController', function(AuthenticationService, $location, $scope){
+    $scope.user = {
+        name: '',
+        password: ''
     };
+
+    $scope.signup = function(){
+        AuthenticationService.register($scope.user).then(function(msg){
+            $location.path('/login');
+        }, function(err){
+            console.log(err);
+        })
+    }
 });
